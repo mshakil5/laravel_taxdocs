15f81bceb2020c1c7f6612ac7754033e3358e9c8
mshakil5 <kazimuhammadullah@gmail.com>
2023-04-09 01:05:07 +0600

    plan css add

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -136,8 +136,44 @@
           </div>
       </div>
   </fieldset> --}}
-  
+
+  <div class="row mb-3">
+    <div class="col-md-12">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    </div>
 </div>
+
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div class="col">
+      <div class="card shadow-sm">
+        <img src="{{ asset('1.png')}}" class="img-responsive" alt="">
+        <div class="card-body">
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          
+        </div>
+      </div>
+    </div>
+    <div class="col">
+        <div class="card shadow-sm">
+            <img src="{{ asset('1.png')}}" class="img-responsive" alt="">
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+            <img src="{{ asset('1.png')}}" class="img-responsive" alt="">
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            
+          </div>
+        </div>
+      </div>
+</div>
+
+
 
 
 @endsection
